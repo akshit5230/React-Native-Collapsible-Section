c4cc498d46508aef9ea476a635731076882783ff
akshit5230 <43376244+akshit5230@users.noreply.github.com>
2019-06-03 18:53:47 +0530

    Update CollapsibleSection.js

--- CollapsibleSection.js
+++ CollapsibleSection.js
@@ -1,39 +1,24 @@
 import React from 'react';
 import {
-  Text,
   View,
-  Image,
   TouchableOpacity,
   Animated
 } from 'react-native';
-import {
-  SemiBoldTextOfSize17
-} from '../CustomComponents';
-
 
 class CollapsibleSection extends React.Component {
   constructor(props) {
     super(props);
-
-    // this.icons = {
-    //   'minus': global_images.right_arrow_gray,
-    //   'plus': global_images.dropdown_gray
-    // };
-    // console.log("props.title >>>> ", props.title);
     this.state = {
       title: props.title,
       expanded: false,
-      minHeight: null, //props.heightOfItem,
+      minHeight: null,
       maxHeight: null,
-      // maxHeight: (props.numberofitems * props.heightOfItem) + props.numberofitems,
       animation: null
     };
   }
 
   componentWillReceiveProps(nextProps) {
-    // console.log("Props are -> ", nextProps)
     this.setState({
-      //   title: nextProps.title,
       refreshView: nextProps.refreshView
     });
   }
@@ -57,7 +42,6 @@ class CollapsibleSection extends React.Component {
   }
 
   _setMaxHeight(event) {
-    // console.log('maxheight setting to ', event.nativeEvent.layout.height)
     let { maxHeight } = this.state
     if (!maxHeight) {
       this.setState({
@@ -67,7 +51,6 @@ class CollapsibleSection extends React.Component {
   }
 
   _setMinHeight(event) {
-    // console.log('minheight setting to ', event.nativeEvent.layout.height)
     let { minHeight } = this.state
     if (!minHeight) {
       this.setState({
@@ -79,45 +62,16 @@ class CollapsibleSection extends React.Component {
   }
 
   render() {
-    // let icon = this.icons['plus'];
-
-    // if (this.state.expanded) {
-    //   icon = this.icons['minus'];
-    // }
-    let { item, onPressEdit, onPressDelete, title, access } = this.props
-    // console.log('access is', access)
-    //Step 5
     return (
       <Animated.View style={[styles.container, { height: this.state.animation }]} >
         <TouchableOpacity
-          style={styles.titleContainer}
           activeOpacity={0.8}
           onPress={this.toggle.bind(this)}
           onLayout={this._setMinHeight.bind(this)}
         >
-          <View style={styles.header}>
-            <SemiBoldTextOfSize17 text={title} style={{ flex: 1, paddingRight: 10, color: "#596D88" }} />
-            {access == 'Full Action' ?
-              <View style={{
-                flexDirection: 'row'
-              }}>
-                <TouchableOpacity
-                  activeOpacity={1}
-                  onPress={() => onPressEdit(item)}>
-                  <Image source={images.edit_blue} />
-                </TouchableOpacity>
-                <TouchableOpacity
-                  style={{ marginLeft: 10 }}
-                  activeOpacity={1}
-                  onPress={() => onPressDelete(item)}>
-                  <Image source={images.icon_delete} />
-                </TouchableOpacity>
-              </View>
-              : null}
-          </View>
+          {this.props.header}
         </TouchableOpacity>
-        <View style={{ height: 2, backgroundColor: '#f7f7f7' }} />
-        <View style={styles.body} onLayout={this._setMaxHeight.bind(this)}>
+        <View onLayout={this._setMaxHeight.bind(this)}>
           {this.props.children}
         </View>
       </Animated.View>
@@ -134,56 +88,8 @@ class CollapsibleSection extends React.Component {
 
 const styles = {
   container: {
-    backgroundColor: 'white',
-    marginVertical: 4,
     overflow: 'hidden',
-    // shadowOffset: { width: 0, height: 0.5, },
-    // shadowColor: 'grey',
-    // shadowOpacity: 0.2,
-    // elevation: 2,
-    // shadowRadius: 2,
-    // height: 40
-  },
-  titleContainer: {
-    // flexDirection: 'row',
-    // height: 50,
-    // justifyContent: 'space-between'
-  },
-  title: {
-    flex: 1,
-    padding: 10,
-    color: '#2a2f43',
-    fontWeight: 'bold',
-  },
-  button: {
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  buttonImage: {
-    width: 25,
-    height: 25,
-    marginRight: 5
-  },
-  body: {
-    padding: 10,
-    paddingTop: 0
-  },
-  txtTitle: {
-    // fontFamily: "$font_medium",
-    // flex: 1,
-    padding: 10,
-    // margin: 12,
-    fontSize: 15,
-    // fontWeight: "500",
-    color: "#3C4042"
-  },
-  header: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    padding: 15,
-    // borderBottomColor: '#DCE3E8',
-    // borderBottomWidth: 0.5,
   }
 }
+
 export default CollapsibleSection;
